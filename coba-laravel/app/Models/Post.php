9f0747d0d8aee10080472d7d18676ca24fb353f4
193040126 <193040126.diva@mail.unpas.ac.id>
2021-10-15 17:13:23 +0700

    menyelesaikan video ketiga laravel dengan topik post model dengan menambahkan beberapa data juga  pada database

--- coba-laravel/app/Models/Post.php
+++ coba-laravel/app/Models/Post.php
@@ -2,32 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts =  [
-            [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Diva Amwal",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error laborum quisquam, aspernatur rerum deleniti veritatis pariatur odit optio, facere similique iure esse repellat quos aliquam at dolore suscipit quibusdam distinctio?"
-            ],
-            [
-            "title" => "Judul Post Pak Doddy",
-            "slug" => "judul-post-kedua",
-            "author" => "Doddy Ferdiansyah",
-            "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, modi? Nemo ex adipisci repellendus reiciendis enim laboriosam officia mollitia magnam molestiae dignissimos autem, nihil accusantium fugit minus quidem placeat quaerat unde debitis pariatur natus soluta illum eos esse vel! Et magni deleniti quis facere dolor eaque asperiores sit laudantium itaque doloremque corrupti nam, esse ipsum quidem animi neque reiciendis, sed minima illum unde sunt facilis! Accusamus saepe atque corporis suscipit eos autem, sint animi corrupti natus tempora voluptatibus maiores velit neque consequatur ipsam dolor adipisci quaerat ipsum aut repellat illum ex commodi consectetur alias. Repellat quos deleniti autem accusamus aliquam."
-            ]
-    ];
+    use HasFactory;
 
-    public static function all(){
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug){
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
