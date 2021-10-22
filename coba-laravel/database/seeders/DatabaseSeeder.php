0202a78cb071e738450075eeef4330ccc68ded64
193040126 <193040126.diva@mail.unpas.ac.id>
2021-10-22 15:45:31 +0700

    menyelesaikan video kedua pertemuan 7 dengan topik belajar laravel:factory&faker

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -17,19 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+    
 
-        User::create([
-            'name' => 'Diva Amwal',
-            'email' => '193040126.diva@mail.unpas.ac.id',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Diva Amwal',
+        //     'email' => '193040126.diva@mail.unpas.ac.id',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Doddy Ferdiansyah',
-            'email' => 'doddy@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Doddy Ferdiansyah',
+        //     'email' => 'doddy@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Prgramming',
@@ -41,40 +43,42 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Keempat',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque porro minus nesciunt non labore nostrum, quis reprehenderit quisquam quae, expedita sapiente! Asperiores fugit soluta similique voluptatum, quibusdam, itaque sunt animi eligendi id, voluptatem perferendis! Similique repellat, dolor, expedita magni molestiae perferendis reprehenderit ea eum dolorem laboriosam doloremque autem esse iste animi magnam quasi voluptas ad ducimus tempore eligendi id? Sunt fugit tempore hic nulla veritatis repudiandae aperiam accusantium quidem. Iure voluptates, blanditiis fugiat impedit, nam natus deserunt dolore suscipit nobis odio voluptatem ipsam id, vitae optio eos accusamus alias quo illo! Expedita dicta saepe optio eligendi temporibus quia? Fugiat, placeat.',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
