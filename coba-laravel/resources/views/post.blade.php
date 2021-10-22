8105bbbed845a76c410a73ea30eb0650ef12d9a2
193040126 <193040126.diva@mail.unpas.ac.id>
2021-10-22 10:16:43 +0700

    menyelesaikan video pertama pertemuan ke 7 dengan topik belajar laravel database seeder

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -5,9 +5,12 @@
         
     </article> --}}
     <h2>{{ $post->title }}</h2>
-    <p>By. Diva Amwal in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">
+            {{ $post->category->name }}
+           </a>
+    </p>
         {{-- <h5>{{ $post["author"] }}</h5> --}}
     {!! $post->body !!}
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
     
 @endsection
