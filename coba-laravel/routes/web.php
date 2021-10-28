a99f893ccdda8342a06b30c78a05e16a703dfd4e
193040126 <193040126.diva@mail.unpas.ac.id>
2021-10-29 00:18:19 +0700

    Menyelesaikan video kedua pertemuan 8 dengan topik Laravel:View Login & Registration

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
+use App\Models\Category;
 
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
-use App\Models\Category;
-use App\Models\User;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 
@@ -53,4 +52,9 @@ Route::get('/categories', function(){
         'categories' => Category::all()
     ]);
 });
+
+
+Route::get('/login', [LoginController::class,  'index']);
+
+Route::get('/register', [RegisterController::class,  'index']);
 
